19c3f4b283fc107e3cf8be78b6f41677052642d1
Peter Dickten <peter.dickten@dcs-fuerth.de>
2026-03-03 14:00:36 +0100

    property binding

--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -1,9 +1,17 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'books-list',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
-export class BookList { }
+export class BookList {
+  books = [
+    { isbn: 123, title: 'Angular 18', price: 19, rating: 3.5, coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._AC_UY218_.jpg' },
+    { isbn: 124, title: 'Angular 19', price: 22, rating: 3.6, coverUrl: 'https://m.media-amazon.com/images/I/71wlgd2ShsL._AC_UY218_.jpg' },
+    { isbn: 125, title: 'Angular 12', price: 25, rating: 3.7, coverUrl: 'https://m.media-amazon.com/images/I/71le4bCnY1L._AC_UY218_.jpg' },
+  ];
+
+}
